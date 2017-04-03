d38fe1936959518f269ffd44e4b22130c6ea0b53
Stefan <stefanpopov@list.ru>
2017-04-04 01:48:32 +0400

    modify structure

---
+++ View/AddFunction.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Charting
+{
+    /// <summary>
+    /// Логика взаимодействия для AddFunction.xaml
+    /// </summary>
+    public partial class AddFunction : Window
+    {
+        public string Name;
+        public string Function;
+        public int Step;
+        public double Min;
+        public double Max;
+        public bool IsOK;
+
+        public AddFunction()
+        {
+            InitializeComponent();
+            IsOK = false;
+        }
+        
+        private void ok_Button_Click(object sender, RoutedEventArgs e)
+        {
+            Name = name_txt.Text;
+            Function = function_txt.Text;
+            if (!Int32.TryParse(step_txt.Text, out Step))
+            {
+                MessageBox.Show("Поле 'шаг' задано не корректно!");
+                return;
+            }
+            if (!Double.TryParse(min_txt.Text, out Min))
+            {
+                MessageBox.Show("Поле 'min' задано не корректно!");
+                return;
+            }
+            if (!Double.TryParse(max_txt.Text, out Max))
+            {
+                MessageBox.Show("Поле 'max' задано не корректно!");
+                return;
+            }
+            if (String.IsNullOrEmpty(Name) || String.IsNullOrEmpty(Function))
+            {
+                MessageBox.Show("Поля 'название' и 'функция' не заданы!");
+                return;
+            }
+            IsOK = true;
+            this.Close();
+        }
+    }
+}
---
+++ View/ChartingView.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Charting
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class ChartingView : Window
+    {
+        private ChartingController controller;
+
+        public ChartingView()
+        {
+            InitializeComponent();
+            controller = new ChartingController();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //// Все графики находятся в пределах области построения ChartArea, создадим ее
+            //chart.ChartAreas.Add(new ChartArea("Default"));
+
+            //// Добавим линию, и назначим ее в ранее созданную область "Default"
+            //chart.Series.Add(new Series("Series1"));
+            //chart.Series["Series1"].ChartArea = "Default";
+            //chart.Series["Series1"].ChartType = SeriesChartType.Line;
+
+            //// добавим данные линии
+            //string[] axisXData = new string[] { "a", "b", "c" };
+            //double[] axisYData = new double[] { 0.1, 1.5, 1.9 };
+            //chart.Series["Series1"].Points.DataBindXY(axisXData, axisYData);
+
+        }
+
+        private void addFunction_Click(object sender, RoutedEventArgs e)
+        {
+            AddFunction f = new AddFunction() { Title = "Добавление" }; 
+            f.ShowDialog();
+            if (!f.IsOK)
+                return;
+            controller.addFunction(f.Name, f.Function, f.Step, f.Min, f.Max);
+
+            GraphicsElement ge = new GraphicsElement(controller.getAllChartings().Count, 
+                                                     f.Name, f.Function, f.Step, f.Min, f.Max);
+            
+            //frontPanel.Height += ge.Height;
+            //elementsPanel.Height += ge.Height;
+            ge.Margin = new Thickness(1);
+            ge.Width = elementsPanel.Width - scrollViewer.Width;
+            elementsPanel.Children.Add(ge);
+            
+        }
+
+        private void alterFunction_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        
+    }
+
+
+}
